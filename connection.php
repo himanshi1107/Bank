cf66b6c42745481128df1fcadcdfe3b0aba0de56
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2021-05-19 20:59:10 +0530

    Update Connection

--- connection.php
+++ connection.php
@@ -1,9 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "bankusers";
+// Remote Database Connection
+$servername = "remotemysql.com";
+$username = "hr22Db2UlY";
+$password = "lkBFBdPrDP";
+$db = "hr22Db2UlY";
+
+// Development Connection
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "bankusers";
 
 $conn = mysqli_connect($servername, $username, $password, $db);
 
